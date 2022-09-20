7a0da1f3ce2668a2f4321f8823bf724dac45cd6d
aysenurkaplan <kaysenur551@gmail.com>
2022-09-20 13:40:50 +0300

    Hello world, değişkenler ve temel veri tipleri.

---
+++ Homeworks/src/Main.java
@@ -0,0 +1,25 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Merhaba java");
+		System.out.println("Merhaba java 2");
+		
+		int ogrenciSayisi = 11;
+		String metin = "Öğrenci sayısı :";
+		
+		System.out.println(metin + ogrenciSayisi);
+		System.out.println(metin + ogrenciSayisi);
+		System.out.println(metin + ogrenciSayisi);
+		
+		double sayi = 12.5;
+		sayi = -129;
+		int a = 78;
+		char karakter = 'A';
+		
+		boolean dogruMu = false;
+		System.out.println(sayi+","+ a +","+ karakter);
+
+	}
+
+}

74a5845bce653e764a3536c97007fda1468b3f11
aysenurkaplan <kaysenur551@gmail.com>
2022-09-20 14:11:59 +0300

    ödeve devam

---
+++ Homeworks/src/conditionals.java
@@ -0,0 +1,18 @@
+
+public class conditionals {
+
+	public static void main(String[] args) {
+		int sayi=38;
+		if(sayi<15) {
+			System.out.println("sayi küçüktür 15'ten");
+		}
+		else if(sayi>15) {
+			System.out.println("sayi büyüktür 15'ten");
+		}
+		else {
+			System.out.println("sayi eşittir 15 e");
+		}
+
+	}
+
+}
